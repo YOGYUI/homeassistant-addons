2210b4e0efbfaab587492f64008097988798ad91
YOGYUI <lee2002w@gmail.com>
2024-03-18 17:40:40 +0900

    - deactivate ingress - add options for application arguments

--- homenet-hillstate/run.sh
+++ homenet-hillstate/run.sh
@@ -3,5 +3,16 @@ repo_path=/repos/yogyui/HomeNetwork
 echo "try to update repository"
 git pull
 
+# read `options` from config
+CONFIG_MQTT_BROKER="$(bashio::config 'mqtt_broker')"
+# CONFIG_RS485="$(bashio::config 'rs485')"
+CONFIG_RS485="[$(echo $(bashio::config 'rs485') | sed -e 's/} /},/g')]"  # parse dict list to json list format
+CONFIG_DISCOVERY="$(bashio::config 'discovery')"
+CONFIG_PARSER_MAPPING="$(bashio::config 'parser_mapping')"
+
 source ${repo_path}/Hillstate-Gwanggyosan/activate.sh
-python3 ${repo_path}/Hillstate-Gwanggyosan/app.py
+python3 ${repo_path}/Hillstate-Gwanggyosan/app.py \
+  --mqtt_broker=$CONFIG_MQTT_BROKER \
+  --rs485=$CONFIG_RS485 \
+  --discovery=$CONFIG_DISCOVERY \
+  --parser_mapping=$CONFIG_PARSER_MAPPING
